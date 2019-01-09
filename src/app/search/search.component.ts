552a1beb50d757bc0873ad302658f6b4b18b3c06
Tatiana Molina <tatymoly@Tatianas-MacBook-Air.local>
2019-01-09 12:49:48 -0500

    changed everything

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -5,137 +5,53 @@ import { startWith, map } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
 import { BestPriceService } from '../shared/services/best-price.service';
 
-export interface CountryGroup {
-  letter: string;
-  names;
-}
-export const _filterCity = (item: string, value: string) => {
-  const filterValue = value.toLowerCase();
-  return item.toLowerCase().includes(filterValue);
-};
-
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
-  countryForm: FormGroup = this.fb.group({
-    originGroup: '',
-    destinationGroup: '',
-  });
-
-  destinationForm: FormGroup = this.fb.group({
-    countryGroup: '',
-  });
-
-  countryGroups: CountryGroup[] = [
+  countryGroup = [
     {
-      letter: 'AR',
-      names: [
-        {
-          city: 'Buenos Aires',
-          iataCityCode: 'BAS',
-        },
-        {
-          city: 'Mendoza',
-          iataCityCode: 'MDZ',
-        },
-      ],
+      country: 'CO',
     },
     {
-      letter: 'BR',
-      names: [
-        {
-          city: 'Rio de Janeiro',
-          iataCityCode: 'RIO',
-        },
-        {
-          city: 'Sao Pablo',
-          iataCityCode: 'SAO',
-        },
-      ],
+      country: 'CL',
+    },
+  ];
+  cityGroup = [
+    {
+      name: 'Bogotá',
+      iataCode: 'BOG',
     },
     {
-      letter: 'CL',
-      names: [
-        {
-          city: 'Santiago de Chile',
-          iataCityCode: 'SCL',
-        },
-      ],
-    },
-    {
-      letter: 'CO',
-      names: [
-        {
-          city: 'Bogotá',
-          iataCityCode: 'BOG',
-        },
-        {
-          city: 'Medellín',
-          iataCityCode: 'MDE',
-        },
-      ],
-    },
-    {
-      letter: 'ES',
-      names: [
-        {
-          city: 'Madrid',
-          iataCityCode: 'MAD',
-        },
-        {
-          city: 'Barcelona',
-          iataCityCode: 'BCN',
-        },
-      ],
-    },
-    {
-      letter: 'MX',
-      names: [
-        {
-          city: 'Ciudad de México',
-          iataCityCode: 'MEX',
-        },
-      ],
-    },
-    {
-      letter: 'PE',
-      names: [
-        {
-          city: 'Lima',
-          iataCityCode: 'LIM',
-        },
-      ],
+      name: 'Medellín',
+      iataCode: 'MDE',
     },
   ];
 
-  countryGroupOptions: Observable<CountryGroup[]>;
-  destinationGroupOpt: Observable<CountryGroup[]>;
-
   travelTypes: string[] = ['Ida y Regreso', 'Solo Ida'];
   travelTpye: string;
-
+  cities: string;
   country: string;
-  destination: string;
-  returnDate: Boolean = false;
-  searchOptions: Object = {
-    country: this.country,
-    destination: this.destination,
+  myObj = {
+    country: '',
+    destination: '',
   };
   myParams;
 
   constructor(
     private route: ActivatedRoute,
-    private fb: FormBuilder,
     private bestPriceService: BestPriceService
-  ) {}
-
-  ngOnInit() {
+  ) {
     this.getUrlParams();
-    this.getCountryGroupOptions();
+    console.log(this.country);
+    console.log(this.cities);
+    this.myObj.country = this.country;
+    this.myObj.destination = this.cities;
   }
+
+  ngOnInit() {}
 
   getUrlParams() {
     this.route.queryParams.subscribe(params => {
@@ -144,44 +60,11 @@ export class SearchComponent implements OnInit {
     });
   }
 
-  getCountryGroupOptions() {
-    // tslint:disable-next-line:no-non-null-assertion
-    this.countryGroupOptions = this.countryForm
-      .get('originGroup')!
-      .valueChanges.pipe(
-        startWith(''),
-        map(value => this._filterGroup(value))
-      );
-    // tslint:disable-next-line:no-non-null-assertion
-    this.countryGroupOptions = this.countryForm
-      .get('destinationGroup')!
-      .valueChanges.pipe(
-        startWith(''),
-        map(value => this._filterGroup(value))
-      );
-  }
-
   sendParamsOptions() {
-    this.bestPriceService.searchBestPrices(this.myParams).subscribe(res => {
+    console.log(this.country);
+    console.log(this.cities);
+    this.bestPriceService.searchBestPrices(this.myObj).subscribe(res => {
       console.log(res);
     });
-  }
-
-  private _filterGroup(value: string): CountryGroup[] {
-    if (value) {
-      return this.countryGroups
-        .map(group => {
-          return {
-            letter: group.letter,
-            names: group.names.filter(
-              item =>
-                _filterCity(item.city, value) ||
-                _filterCity(item.iataCityCode, value)
-            ),
-          };
-        })
-        .filter(group => group.names.length > 0);
-    }
-    return this.countryGroups;
   }
 }
